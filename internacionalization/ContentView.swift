a5e398412a09f993d7e8b938c72078d4c7457d6f
Rodrigo-Youse <rodrigo.amancio@youse.com.br>
2024-06-26 12:46:36 -0300

    create internacionalization to test using a localizable strings

--- internacionalization/ContentView.swift
+++ internacionalization/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    //Change here to see pluralization
+    @State private var itemCount = 1
+    // If you add a new variable, the xcode add automatically to localizabe string and you need to set to another languages
+    @State private var name = "Rodrigo"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(itemCount) Books")
+            Text("Olá \(name)")
+
         }
         .padding()
     }
