dc57025acd5fa0d1a95ee5c830804ff9bc2f6ee5
Rodrigo-Youse <rodrigo.amancio@youse.com.br>
2024-06-26 10:56:36 -0300

    Initial Commit

---
+++ internacionalization/internacionalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  internacionalizationApp.swift
+//  internacionalization
+//
+//  Created by Rodrigo Silva on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct internacionalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
